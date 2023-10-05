55cd5c71c1fc008470ec88b508fc423015a15088
Chirag Matkar <chirag.matkar@gmail.com>
2023-10-06 00:26:38 +0530

    fastapi

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import uvicorn
+import os
 
 app = FastAPI()
 
@@ -6,3 +8,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
+ # at last, the bottom of the file/module
+if __name__ == "__main__":
+    port = os.environ.get('FLASK_PORT') or 8080
+    port = int(port)
+    uvicorn.run(app, host='0.0.0.0',port=port)
+    
